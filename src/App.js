49874596197e17afada8a3097fdaac8d1c632159
tmicka23 <mickathp42@gmail.com>
2020-04-12 15:35:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to <br />
+          Mickaël Thely's Portfolio
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strng>This application is under building ...</strng>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
